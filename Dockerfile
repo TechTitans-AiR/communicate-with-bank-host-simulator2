873d852c1381f619465c9f42cd280dd2fe9acbcd
GrgoPenava <grgopenava00@gmail.com>
2024-01-28 22:28:16 +0100

    Change dockerfile, Remove packages from python file, Remove packages from requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,6 @@ RUN apt-get update && apt-get install -y --no-install-recommends libssl-dev libf
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-ARG MONGO_URI
-RUN echo "MONGO_URI=${MONGO_URI}" > .env
-
 COPY . /app
 
 CMD ["python", "bankCommunication.py"]
--- bankCommunication.py
+++ bankCommunication.py
@@ -1,18 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
-from pymongo import MongoClient
-from dotenv import load_dotenv
 import requests
 import json
 import os
 
-
-load_dotenv()
-
 app = Flask(__name__)
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-
-mongo = PyMongo(app)
 
 @app.route('/sendToBankHostSimulator2', methods=['POST'])
 def handle_transaction():
